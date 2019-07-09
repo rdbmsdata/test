d5393d4f406feee2a9723fc67c3e5a005ed48e62
rdbmsdata <rdbmsdata.ranjan@gmail.com>
2019-07-09 16:04:35 -0400

    Add files via upload

---
+++ src/main/java/spring/oshift/SpringOshiftApplication.java
@@ -0,0 +1,13 @@
+package spring.oshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/oshift/SpringOshiftApplicationTests.java
@@ -0,0 +1,16 @@
+package spring.oshift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringOshiftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
